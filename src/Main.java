b2852f1c2dd00d83e0487d6248cd8ca9baec37e8
stan10 <s97754@pollub.edu.pl>
2024-10-10 09:22:33 +0200

    feat/numer
    Numer o numerze o numerze

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         for (int i = 1; i <= 20; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("Numer elllo = " + i);
+            System.out.println("Numer o numerze o numerze = " + i);
         }
     }
 }
