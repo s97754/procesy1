75c97433272ee08c7f9eab7d780902c9de46209b
stan10 <s97754@pollub.edu.pl>
2024-10-10 09:23:16 +0200

    feat/id
    Wypisywanie ID pracownikow

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Wypisywanie ID pracownikow!");
 
         for (int i = 1; i <= 20; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("Numer elllo = " + i);
+            System.out.println("Numer o ID = " + i);
         }
     }
 }
